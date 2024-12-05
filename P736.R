93e946b72cb8f847f9a721b162cf8d56a14f9079
HL <liu.haoxuan@outlook.com>
2024-12-05 20:54:27 +0000

    Modelling part half complete

--- P736.R
+++ P736.R
@@ -36,6 +36,12 @@ sd <- sapply(df[c("docvis", "age", "hhninc", "educyrs")], sd)
 print(means)
 print(1-means)
 
+sum(df$docvis == 0) / nrow(df)
+sort(table(df$educyrs), decreasing = TRUE)
+male <- mean(df$docvis[df$female == "Male"], na.rm = TRUE)
+female <- mean(df$docvis[df$female == "Female"], na.rm = TRUE)
+male - female
+
 # univariate
 hist_female <- ggplot(df, aes(x = docvis, fill = female)) +
   geom_histogram(aes(y = ..density..), position = "dodge", binwidth = 1) +
@@ -78,24 +84,28 @@ density_age <- ggplot(df, aes(x = age)) +
   geom_density(col = "red", size = 1) +
   labs(y = "Density", x = "Age (Years)")
 
-plot_2 <- grid.arrange(density_hhninc, density_educyrs, density_age, ncol = 3)
+plot_2 <- grid.arrange(density_hhninc, density_age, density_educyrs, ncol = 3)
 
 # bivariate
 
 ggplot(df, aes(y = docvis, x = educyrs)) +
   geom_point(alpha = 0.3, position = position_jitter(width = 0.1)) +
   facet_grid(~ female) +
+  geom_smooth(color = "red", se = FALSE, size = 0.75)
   labs(y = "Visits to a doctor", x = "Years of Schooling")
 
-ggplot(df, aes(y = docvis, x = hhninc)) +
+plot_3 <- ggplot(df, aes(y = docvis, x = hhninc)) +
   geom_point(alpha = 0.3, position = position_jitter(width = 0.1)) +
   facet_grid(~ female) +
+  geom_smooth(color = "red", se = FALSE, size = 0.75)
   labs(y = "Visits to a doctor", x = "Net monthly household income (German Marks/100)")
 
-ggplot(df, aes(y = docvis, x = age)) +
+plot_4 <- ggplot(df, aes(y = docvis, x = age)) +
   geom_point(alpha = 0.3, position = position_jitter(width = 0.1)) +
   facet_grid(~ female) +
+  geom_smooth(color = "red", se = FALSE, size = 0.75) +
   labs(y = "Visits to a doctor", x = "Age (Years)")
+
 
 boxplot_married <- ggplot(df, aes(y = docvis, x = married, fill = female)) +
   geom_boxplot() +
@@ -125,5 +135,44 @@ layout <- rbind(c(1, 2),
                 c(3, 4),
                 c(5, 5))
 
-plot_3 <- grid.arrange(boxplot_married, boxplot_hhkids, boxplot_addins, boxplot_privateins, boxplot_eductype, layout_matrix = layout)
-grid.arrange(plot_3, boxplot_eductype, col = 1)
+plot_4 <- grid.arrange(boxplot_married, boxplot_hhkids, boxplot_addins, boxplot_privateins, boxplot_eductype, layout_matrix = layout)
+
+boxplot_married2 <- ggplot(df, aes(y = docvis, x = married)) +
+  geom_boxplot() +
+  labs(y = "Visits to a doctor", x = "Relationship")
+
+boxplot_employed2 <- ggplot(df, aes(y = docvis, x = employed)) +
+  geom_boxplot() +
+  labs(y = "Visits to a doctor", x = "Employment")
+
+boxplot_hhkids2 <- ggplot(df, aes(y = docvis, x = hhkids)) +
+  geom_boxplot() +
+  labs(y = "Visits to a doctor", x = "Child in the family")
+
+boxplot_addins2 <- ggplot(df, aes(y = docvis, x = addins)) +
+  geom_boxplot() +
+  labs(y = "Visits to a doctor", x = "Additional Insurance")
+
+boxplot_privateins2 <- ggplot(df, aes(y = docvis, x = privateins)) +
+  geom_boxplot() +
+  labs(y = "Visits to a doctor", x = "Private Insurance")
+
+boxplot_eductype2 <- ggplot(df, aes(y = docvis, x = eductype)) +
+  geom_boxplot() +
+  labs(y = "Visits to a doctor", x = "Highest Level of Schooling Obtained")
+
+grid.arrange(boxplot_employed2, boxplot_hhkids2, boxplot_addins2, ncol = 3)
+
+
+# Modelling
+df$age = df$age - mean(df$age)
+df$hhninc = df$hhninc - mean(df$hhninc)
+df$educyrs = df$educyrs - mean(df$educyrs)
+
+glm_1 <- glm(docvis ~ ., family = poisson, data = df)
+summary(glm_1)
+
+glm_2 <- glm(docvis ~ . + . * female, family = poisson, data = df)
+summary(glm_2)
+
+glm_3 <- step(glm_2, trace = T)
